940c49488b72eb6fdada1052e1bfb916d8863ba2
ze666ro <jesus666saves@hotmail.com>
2023-11-14 19:18:35 +0300

    day 6-1

--- service/services/views.py
+++ service/services/views.py
@@ -12,8 +12,7 @@ class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related(
         'plan',
         Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email'))
-    ).annotate(price=F('service__full_price') -
-                     F('service__full_price') * F('plan__discount_percent') / 100.00)
+    )#
     serializer_class = SubscriptionSerializer
 
 
